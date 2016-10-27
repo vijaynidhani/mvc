8696f4150f8c4889241ace1c7a1d5eadc6c4088a
vijay <profes.vijay@gmail.com>
2016-10-26 22:24:13 -0700

    testtestte

--- gs-serving-web-content-complete/src/main/java/hello/GreetingController.java
+++ gs-serving-web-content-complete/src/main/java/hello/GreetingController.java
@@ -11,6 +11,7 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";   
+        System.out.println();
+        return "greeting";      
     }    
 }
