7ddd067914911570819cc8a7c76b4b7daa2edbda
vijay <profes.vijay@gmail.com>
2016-10-26 22:34:40 -0700

    Changes

--- gs-serving-web-content-complete/src/main/java/hello/GreetingController.java
+++ gs-serving-web-content-complete/src/main/java/hello/GreetingController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        System.out.println();
+        model.addAttribute("name", name); 
         return "greeting";      
     }    
 }
