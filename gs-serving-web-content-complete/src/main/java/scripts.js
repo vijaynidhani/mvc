8bd0018a975def829d0fc4f85672d091724ca027
vijay <profes.vijay@gmail.com>
2016-10-27 01:00:59 -0700

    Removed

--- gs-serving-web-content-complete/src/main/java/scripts.js
+++
@@ -1,8 +0,0 @@
- var scotchApp = angular.module('scotchApp', []);
-
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
